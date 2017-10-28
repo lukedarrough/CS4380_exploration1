70df699700ab08230788dd06de5cac9269d799ab
lukedarrough <lukedarrough@yahoo.com>
2017-10-27 21:28:11 -0500

    test comment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game.js';
 import './index.css';
-
+//root of the project
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
